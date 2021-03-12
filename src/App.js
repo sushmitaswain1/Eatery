449819e673c7135f87b4024c1eb18d6797643345
Sushmita Swain <sushmita905@gmail.com>
2021-03-12 18:47:07 +0530

    Added google fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Raleway', sans-serif;
+  }
+
+h1, h2, h3 {
+  font-family: 'Roboto Condensed', sans-serif;
+}
+`
+
 
 function App() {
   return (
+    <>
+    <h1>Zaaika</h1>
+    <GlobalStyle/>
     <div className="App">
       Hello world
       
     </div>
+    </>
   );
 }
 
