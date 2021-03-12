0881901cae63a97fa3b8304ee1991a30fc0efe8d
Sushmita Swain <sushmita905@gmail.com>
2021-03-12 20:43:21 +0530

    added Navbar and Title styles

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import {mainColor} from '../Styles/Colors';
+import {Title} from '../Styles/Titles';
+
+const NavbarStyled = styled.div`
+    background-color: ${mainColor};
+    padding : 10px;
+
+`
+const Logo = styled(Title)`
+font-size: 25px;
+color: white;
+text-shadow: 2px 2px 5px #00001a;
+
+`
+export function Navbar() {
+    return(
+        <NavbarStyled>
+           <Logo>
+               Zaaika 
+           </Logo>
+        </NavbarStyled>
+
+    );
+}
+
